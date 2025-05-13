f2267abdcb35b928409bbc53a84af8deb0383efc
Matthew Neba <mattneba4343@gmail.com>
2025-05-13 16:44:40 -0600

    Completed second hard problem

--- sliding_window/sliding_window_max.py
+++ sliding_window/sliding_window_max.py
@@ -3,11 +3,43 @@ from collections import deque
 
 # Originally, was gonna use a min-heap to do a varaition of the top-k algorithm to get an O(n * log(k)) time and O(k) space algorithm. But a better time complexity with same space complexity exists with a deque. To get this solution, the key observation was that if we find that a value after an index in the window is larger that the ones before, will never need to consider any of those previous indices again.
 
+# ! To To help with the implementation of all algorithms, imagine yourself as the computer and what needs to be done on each step. Greatly helps
+
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
 
-        # instead , add elements one at a time
+        if len(nums) < k:
+            return [max(nums)]
+        
+        l = r = 0
+        max_elems = []
+        window_queue = deque()
 
+        while r < len(nums):
+            while ((window_queue) and (nums[r] > window_queue[-1])):
+                window_queue.pop()
+            
+            window_queue.append(nums[r])
+            r += 1
+
+            if r < k:
+                continue
+            
+            max_elems.append(window_queue[0])
+
+            if nums[l] == window_queue[0]:
+                window_queue.popleft()
+            
+            l += 1
+
+        return max_elems
+
+            
+
+
+solution = Solution()
+
+solution.maxSlidingWindow([1,2,1,0,4,2,6], 3)
 
             
 
