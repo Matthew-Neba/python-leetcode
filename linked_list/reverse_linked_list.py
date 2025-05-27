ca39be87e802aee8d1fdc32dbcd6ce4e1a2ca659
Matthew Neba <mattneba4343@gmail.com>
2025-05-26 19:18:10 -0600

    Reorder linked list complete:

--- linked_list/reverse_linked_list.py
+++ linked_list/reverse_linked_list.py
@@ -1,22 +1,42 @@
 from typing import Optional
 
+# Key here is realizing problem can be broken down into three steps. 1) Split linked list in half, 2) reverse second half, 3) merge them together. Another key idea here is the use fast and slow pointers to get the middle of the linked list in O(n/2) time
+
 class ListNode:
      def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
 
 class Solution:
-    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        prevNode = None
-        curNode = head
+    def reorderList(self, head: Optional[ListNode]) -> None:
 
-        while curNode != None:
-            nextNode = curNode.next
-            curNode.next = prevNode
-            prevNode = curNode
-            curNode = nextNode
+        # get middle in O(n/2) time
+        slow = fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        middle = slow.next
+        slow.next = None
+
+        # reverse elements after the middle of the linked list
+        prevNode = None
+        while middle:
+            nextNode = middle.next
+            middle.next = prevNode
+            prevNode = middle
+            middle = nextNode
         
-        return prevNode
+        firstHalf, secondHalf = head, prevNode
+
+        # merge together
+        while secondHalf:
+            tmp1, tmp2 = firstHalf.next, secondHalf.next
+            firstHalf.next = secondHalf
+            secondHalf.next = tmp1
+            firstHalf, secondHalf = tmp1, tmp2
+        
+
         
 
         
