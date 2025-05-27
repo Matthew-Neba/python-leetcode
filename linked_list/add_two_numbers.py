990893457fddf30032af95dbc19aa27d3e379519
Matthew Neba <mattneba4343@gmail.com>
2025-05-27 16:58:23 -0600

    Add two numbers completed

---
+++ linked_list/add_two_numbers.py
@@ -0,0 +1,45 @@
+from typing import Optional
+
+
+# Here, can also solve this problem if decide not to create new linked list for the output but just to modify the input linked lists
+
+class ListNode:
+     def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        
+        start = iterNode = ListNode()
+        carry = 0
+        while l1 or l2 or carry:
+            newNode = ListNode()
+            val1 = l1.val if l1 else 0
+            val2 = l2.val if l2 else 0
+
+            newValue = (val1 + val2 + carry) % 10
+            carry = (val1 + val2 + carry) // 10
+
+            newNode.val = newValue
+            iterNode.next = newNode
+            iterNode = iterNode.next
+
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+
+        return start.next
+            
+
+
+
+
+
+
+            
+            
+
+
+
+        
--- tester.py
+++
@@ -1,6 +0,0 @@
-# can slice strings using this notation s[start:stop:step], returns a new string since strings are immutable
-s = "asdas"
-s_new = s[::-1]
-print(s_new)
-
-print(type('s'))
