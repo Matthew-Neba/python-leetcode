7ead66466b03a7006a0f5d4c304b5d7316f8b895
Matthew Neba <mattneba4343@gmail.com>
2025-08-07 00:58:31 -0600

    Started backtracking, subsets completed

---
+++ backtracking/subsets.py
@@ -0,0 +1,48 @@
+from typing import List
+
+#  Solution cannot be better than 2^n since there are 2^n possible subsets for any set, and we need to atleast create those subsets. We would also like a list of lists. This suggests backtracking may be viable here.
+
+
+# O(n * 2^n) time complexity, 2*n recursive calls since 2^n nodes in a full decision tree, and for each recursive call, we are copying path O(n)
+# O(n + n) + O(n) space complexity, n for the recursive stack other n for the space to store path since it is being reused (popping and reusing it) 
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = []
+
+        # very similar to dfs, path is like the root node here
+        def backtrack(path, choices_idx):
+            nonlocal res
+            if choices_idx == len(nums):
+                # reached a leaf node, one set of choices out of all possible ones 
+                # remember python passes in by value, need to copy the path before appending it to res
+                res.append(path[::])
+                return
+          
+            # explore the decision tree further, only two choices in the decision tree split point, include the current value or not
+
+            # include the current choice
+            path.append(nums[choices_idx])
+            backtrack(path, choices_idx + 1)
+
+            # pop off the current choice and continue exploring the other decision tree path (without the current value)
+            path.pop()
+            backtrack(path, choices_idx + 1)
+
+            return
+
+        backtrack([],0)
+        return res
+
+        
+
+
+
+
+
+
+            
+
+
+        
+
+        
--- backtracking/tips.md
+++ backtracking/tips.md
@@ -1,5 +1,20 @@
-- Backtracking is essentially a smarter brute-force with early exits. Use it when the brute force solution would try all possibilities — but you can prune bad paths early
+-   Backtracking is a last resort apart, it is just a smarter brute-force with early exits. Use it when no other better soluution exists (DP, Greedy, Mathematical, etc.)
 
--Backtracking is a last resort apart from unoptimzied brute force. Use it when no other better soluution exists (DP, Greedy, Mathematical, etc.)
+-   Backtracking Algorithm is just a way to explore decision trees
 
-- Key tip is to use a for/while loop to manage the decision making part of each recursion call in backtracking when possible. No need for very complex recursion logic.
+-   We generally use the path variable to keep track of the current value of the decision tree node. We usually also use another variable to check what choices we have left to make (sometiems no variable needed for this, ex: Word Search).
+
+-   We use the base case to check when we should stop exploring the decision tree or when we have reached a leaf node in the tree (i.e: considered all possible choices).
+
+-   General template:
+    def backtrack(path, choices):
+
+        if base_case_condition(path):
+            res.append(path[:])  # Make a copy
+            return
+
+        for choice in choices:
+            if is_valid(choice, path):
+                path.append(choice)  # Make choice
+                backtrack(path, updated_choices)     # Explore further
+                path.pop()                           # Undo choice
